d2f34a62282ba620c1d9167d3ed4e2d736c0d3d4
Govindarajan Thirumalai <gothirum@oradev.oraclecorp.com>
2017-11-05 21:15:06 +0530

    Commented Toast.

--- js/index.js
+++ js/index.js
@@ -433,7 +433,7 @@ function insertDirectionIcon (trafficIcon) {
 }
 
 function speakDirection() {
-	toast('Distance ' + km_distance + ' Leg: ' + leg + ' I Distance ' + _instructions[leg].distance);
+	//toast('Distance ' + km_distance + ' Leg: ' + leg + ' I Distance ' + _instructions[leg].distance);
 	insertDirectionText(_instructions[leg].instruction + ' in ' + (_instructions[leg].distance - km_distance) + ' kms');
 	if ( km_distance > (_instructions[leg].distance - 0.2) && km_distance < _instructions[leg].distance ) {
 		console.log('Instruction : ' + _instructions[leg].instruction);
