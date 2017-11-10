025dddb97ca13439d861d61c2038fa9af857c7a5
Govindarajan Thirumalai <gothirum@oradev.oraclecorp.com>
2017-11-10 20:15:12 +0530

    ios video bug.

--- js/index.js
+++ js/index.js
@@ -127,12 +127,13 @@ function getPlaylist(channel) {
 
 function showVideo(id) {
 	$('#videologo').hide();
-	//var output = '<iframe width="100%" height="250" src="https://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>';
-	var output = '<iframe width="240" height="180" src="http://www.youtube.com/embed/' + id + '" allowfullscreen></iframe>'
-	
+	var output = '<iframe width="100%" height="250" src="https://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>';
 	if ( navigator.userAgent.match(/(iPhone|iPod|iPad)/)) {
+		output='<video webkit-playsinline id="iosvideo" width="320" height="240" src="https://www.youtube.com/watch?v=' + id + '" controls>Your browser does not support the video tag.</video>'
 		console.log('IOS Showing Video ' + output);
 		$('#player').html(output);
+		var video = document.getElementById('iosvideo');
+		console.log('Video ' + video);
 	} else {
 		console.log('Showing Video ' + output);
 		$('#player').html(output);
